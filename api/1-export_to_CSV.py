5b873169dfa2b7f6d94f0e534e64bcd819292d87
ngicheru-netizen <n.gicheru@alustudent.com>
2026-06-28 11:18:11 +0300

    export to CSV

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Gather data from a REST API about an employee's TODO list and turn data to CSV"""
+
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    employee_ID = sys.argv[1]
+    base = f"https://jsonplaceholder.typicode.com/users/{employee_ID}"
+    user_url = base
+    todos_url = f"{base}/todos"
+
+    user = requests.get(user_url).json()
+    todos = requests.get(todos_url).json()
+    TOTAL_NUMBER_OF_TASKS = len(todos)
+    EMPLOYEE_NAME = user["name"]
+
+    DONE_TASKS = []
+    for todo in todos:
+        if todo["completed"]:
+            DONE_TASKS.append(todo)
+
+    NUMBER_OF_DONE_TASKS = len(DONE_TASKS)
+
+
+data_list = []
+
+for todo in todos:
+    data_list.append(
+        {
+            "USER_ID": employee_ID,
+            "USERNAME": EMPLOYEE_NAME,
+            "TASK_COMPLETED_STATUS": todo["completed"],
+            "TASK_TITLE": todo["title"],
+        }
+    )
+fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+
+with open(f"{employee_ID}.csv", "w", newline="", encoding="utf-8") as file:
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows(data_list)
