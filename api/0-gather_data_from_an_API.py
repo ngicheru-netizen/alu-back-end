8015bfb8aa92e94461c8658214d198ec9ae8ca11
ngicheru-netizen <n.gicheru@alustudent.com>
2026-06-28 09:03:22 +0300

    0

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+employee_ID = sys.argv[1]
+user_url = f"https://jsonplaceholder.typicode.com/users/{employee_ID}"
+todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_ID}/todos"
+
+user = requests.get(user_url).json()
+todos = requests.get(todos_url).json()
+TOTAL_NUMBER_OF_TASKS = len(todos)
+EMPLOYEE_NAME = user["name"]
+
+
+DONE_TASKS = []
+for todo in todos:
+    if todo["completed"]:
+        DONE_TASKS.append(todo)
+
+
+NUMBER_OF_DONE_TASKS = len(DONE_TASKS)
+print(
+    "Employee",
+    EMPLOYEE_NAME,
+    "is done with tasks",
+    f"({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS})",
+    "/",
+    TOTAL_NUMBER_OF_TASKS,
+)
+
+for todo in DONE_TASKS:
+    print(f"\t {todo["title"]}")
+
+
+# if __name__ == "__main__":
+#     pass
